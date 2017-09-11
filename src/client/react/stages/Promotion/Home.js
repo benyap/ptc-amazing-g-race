ffbbc373354129e516b0651bc52961215847fed6
bwyap <bwyap@outlook.com>
2017-09-12 09:45:25 +1000

    [FIX] Fix registration text in the wrong position

--- src/client/react/stages/Promotion/Home.js
+++ src/client/react/stages/Promotion/Home.js
@@ -14,8 +14,8 @@ class Home extends React.Component {
 				<div className='date text padding'>
 					7th December, 2017
 				</div>
-					Registration opens
 				<div className='registration text padding'>
+					Registration opens
 					<br/>
 					<span className='em'>Monday 18th September, 2017</span>
 				</div>
