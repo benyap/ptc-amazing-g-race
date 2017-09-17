5e50f7fa515bd646ef56493c839288089c860241
bwyap <bwyap@outlook.com>
2017-09-18 03:20:14 +1000

    [FIX] Reduce speed of the description animation

--- src/client/react/stages/Promotion/Description.js
+++ src/client/react/stages/Promotion/Description.js
@@ -9,19 +9,19 @@ class Description extends React.Component {
 		return (
 			<div className='event-description'>
 				<div className='details'>
-					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0} duration={0.5}>
+					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0}>
 						<h2>What is the AMAZING&nbsp;<span className='em'>G</span>RACE?</h2>
 					</ScrollAnimation>
-					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0} duration={0.5}>
+					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0}>
 						The End of Year event run by students from Power To Change: <br/>
 						<b> are you ready for the challenge?</b>
 					</ScrollAnimation>
-					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0} duration={0.5}>
+					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0}>
 						Embark on a trek around the streets of Melbourne and participate in a series of challenges 
 						with your team to unlock the path to victory... but you must choose your path wisely.
 						Time is short and resources are limited. Will your team reign victorious over the others?
 					</ScrollAnimation>
-					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0} duration={0.5}>
+					<ScrollAnimation animateOnce animateIn='fadeInUp' offset={0}>
 						<b>More details to come - get excited and stay tuned!</b>
 					</ScrollAnimation>
 				</div>
