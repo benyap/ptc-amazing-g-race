debca74b772a46eccf86315a80a3e73a904cd654
bwyap <bwyap@outlook.com>
2017-10-02 13:16:27 +1100

    [ADD] Add API version to S3

--- src/server/db/s3.js
+++ src/server/db/s3.js
@@ -4,12 +4,14 @@ import S3 from 'aws-sdk/clients/s3';
 const AWS_S3_UPLOAD_BUCKET = process.env.AWS_S3_UPLOAD_BUCKET;
 
 const s3 = new S3({
+	apiVersion: '2006-03-01',
 	accessKeyId: process.env.AWS_S3_USER_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_S3_USER_SECRET_ACCESS_KEY,
 	region: process.env.AWS_S3_REGION
 });
 
 const s3Admin = new S3({
+	apiVersion: '2006-03-01',
 	accessKeyId: process.env.AWS_S3_ADMIN_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_S3_ADMIN_SECRET_ACCESS_KEY,
 	region: process.env.AWS_S3_REGION
