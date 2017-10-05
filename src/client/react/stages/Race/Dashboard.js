489b9ed3f4f54ca5e995c9fecbe2c1ac06550c6b
bwyap <bwyap@outlook.com>
2017-10-05 16:08:39 +1100

    [ADD] Add base components to Race dashboard

--- src/client/react/stages/Race/Dashboard.js
+++ src/client/react/stages/Race/Dashboard.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Switch, Redirect } from 'react-router-dom';
 import Base from '../components/Base';
 import HelpMenu from '../components/HelpMenu';
+import Home from '../pages/Home';
+import Help from '../pages/Help';
+import Instructions from '../pages/Instructions';
+import ImageUploaderTest from '../pages/ImageUploaderTest';
+import NotFound from '../pages/NotFound';
 
 
 const mapStateToProps = (state, ownProps) => {
@@ -22,10 +28,21 @@ class Dashboard extends React.Component {
 			}}/>;
 		}
 
+		const { url } = this.props.match;
+
 		return (
-			<div>
+			<div className='pt-dark'>
 				<Base/>
 				<HelpMenu/>
+				<Switch>
+					<Route exact path={`${url}`} component={Home}/>
+					<Route path={`${url}/instructions`} component={Instructions}/>
+					<Route path={`${url}/feed`} component={null}/>
+					<Route path={`${url}/challenges`} component={null}/>
+					<Route path={`${url}/help`} component={Help}/>
+					<Route path={`${url}/image`} component={ImageUploaderTest}/>
+					<Route component={NotFound}/>
+				</Switch>
 			</div>
 		);
 	}
