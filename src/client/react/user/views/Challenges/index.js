e71ab91434a7b36007cf616cddaf457ee62449d4
bwyap <bwyap@outlook.com>
2017-10-09 19:27:59 +1100

    [ADD] Implement loading of challenges

--- src/client/react/user/views/Challenges/index.js
+++ src/client/react/user/views/Challenges/index.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import autobind from 'core-decorators/es/autobind';
-import { Spinner, Button } from '@blueprintjs/core';
+import { Spinner, Button, Dialog, Intent } from '@blueprintjs/core';
+import { graphql } from 'react-apollo';
+import FormInput from '../../../../../../lib/react/components/forms/FormInput';
+import { getChallenges } from '../../../../graphql/challenge';
+import NotificationToaster from '../../../components/NotificationToaster';
+import ChallengeList from './ChallengeList';
 
 
+const QueryGetChallengesOptions = {
+	name: 'QueryGetChallenges',
+	options: { fetchPolicy: 'cache-and-network' }
+};
+
+@graphql(getChallenges('key group type title description locked teams'), QueryGetChallengesOptions)
 @autobind
 class Challenges extends React.Component {
 	state = {
 		loading: false,
 		showHelp: false,
+		showUnlockChallenge: false,
+		unlockChallengeValue: '',
+		unlockChallengeLoading: false,
+		unlockChallengeError: null
 	}
 
-	toggleShowHelp() {
+	toggleHelp() {
 		this.setState((prevState) => {
 			return { showHelp: !prevState.showHelp };
 		});
+	}
+
+	toggleUnlockChallengePassphrase() {
+		this.setState((prevState) => {
+			return { 
+				showUnlockChallenge: !prevState.showUnlockChallenge, 
+				unlockChallengeError: null, 
+				unlockChallengeValue: '' 
+			};
+		});
+	}
+
+	submitPassphrase() {
+		this.setState({unlockChallengeError: null, unlockChallengeLoading: true});
+		// TODO: Make API call to unlock challenge
+
+		this.setState({showUnlockChallenge: false, unlockChallengeLoading: false});
 	}
 
 	render() {
@@ -24,7 +56,7 @@ class Challenges extends React.Component {
 					<h2>
 						Challenges
 						<Button className='helper-button pt-small pt-minimal pt-intent-warning' iconName='refresh' onClick={this.refresh} disabled={this.state.loading}/>
-						<Button className='helper-button pt-small pt-minimal pt-intent-primary' iconName='help' onClick={this.toggleShowHelp}/>
+						<Button className='helper-button pt-small pt-minimal pt-intent-primary' iconName='help' onClick={this.toggleHelp}/>
 					</h2>
 					{ this.state.showHelp ? 
 						<div className='pt-callout pt-icon-help pt-intent-primary'>
@@ -33,7 +65,33 @@ class Challenges extends React.Component {
 						</div>
 						: null
 					}
+					{ this.props.QueryGetChallenges.getChallenges ?
+						<ChallengeList challenges={this.props.QueryGetChallenges.getChallenges}/> :
+						<div style={{textAlign: 'center'}}>
+							<Spinner className='pt-large'/>
+						</div>
+					}
+					<Button className='pt-fill' text='Unlock a challenge' iconName='unlock' onClick={this.toggleUnlockChallengePassphrase}/>
 				</div>
+
+				{/* Unlock challenge dialog */}
+				<Dialog title='Enter passphrase' isOpen={this.state.showUnlockChallenge} onClose={this.toggleUnlockChallengePassphrase}>
+					<div className='pt-dialog-body'>
+						{this.state.unlockChallengeError ? 
+							<div className='pt-callout pt-intent-danger pt-icon-error'>
+								{this.state.unlockChallengeError}
+							</div>
+							:null}
+						<FormInput id='passphrase' value={this.state.editValue} onChange={this.handleChange} large 
+							intent={this.state.unlockChallengeError ? Intent.DANGER : Intent.NONE}/>
+					</div>
+					<div className='pt-dialog-footer'>
+						<div className='pt-dialog-footer-actions'>
+							<Button onClick={this.toggleUnlockChallengePassphrase} text='Cancel' className='pt-minimal' disabled={this.state.unlockChallengeLoading}/>
+							<Button onClick={this.submitPassphrase} text='Enter' intent={Intent.PRIMARY} loading={this.state.unlockChallengeLoading}/>
+						</div>
+					</div>
+				</Dialog>
 			</main>
 		);
 	}
