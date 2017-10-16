470e800c4474ac024b106781d113034e20b4eb58
bwyap <bwyap@outlook.com>
2017-10-16 14:55:03 +1100

    [ADD] Add a no responses screen

--- src/client/react/admin/views/ResponsesView/index.js
+++ src/client/react/admin/views/ResponsesView/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import autobind from 'core-decorators/es/autobind';
 import { graphql } from 'react-apollo';
+import { NonIdealState } from '@blueprintjs/core';
 import { getResponses } from '../../../../graphql/response';
 import LoadingSpinner from '../../../components/LoadingSpinner';
 import RefreshBar from '../../components/RefreshBar';
@@ -48,18 +49,29 @@ class ResponsesView extends React.Component {
 			}
 		}
 		else if (getResponses) {
-			content = (
-				<div className='view-list'>
-					{ getResponses.map((response) => {
-							return <ResponseCard key={response._id} response={response}/>;
-						}).sort((a, b) => {
-							if (a.props.response.checked && b.props.response.checked) return 0;
-							else if (a.props.response.checked) return 1;
-							else if (b.props.response.checked) return -1;
-							else return 0;
-						})}
-				</div>
-			);
+			if (getResponses.length) {
+				content = (
+					<div className='view-list'>
+						{ getResponses.map((response) => {
+								return <ResponseCard key={response._id} response={response}/>;
+							}).sort((a, b) => {
+								if (a.props.response.checked && b.props.response.checked) return 0;
+								else if (a.props.response.checked) return 1;
+								else if (b.props.response.checked) return -1;
+								else return 0;
+							})}
+					</div>
+				);
+			}
+			else {
+				content = (
+					<div>
+						<div style={{margin:'3rem'}}>
+							<NonIdealState title='No responses' description={`You sure the challenges weren't impossibly hard?`} visual='inbox'/>
+						</div>
+					</div>
+				);
+			}
 		}
 		else if (loading) {
 			content = <LoadingSpinner/>;
