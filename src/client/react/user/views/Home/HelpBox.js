8472ecddd06fd33518de29b1dd2b2433f2fc9069
bwyap <bwyap@outlook.com>
2017-10-17 23:14:19 +1100

    [ADD] Update help box description

--- src/client/react/user/views/Home/HelpBox.js
+++ src/client/react/user/views/Home/HelpBox.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HelpBox = ({showHelp}) => {
 	if (showHelp) {
 		return (
 			<div className='pt-callout pt-icon-help pt-intent-primary'>
-				Here you can see an overview of your team's progress
-				and any important announcements.
+				Here you can see an overview of your team's progress.
 				Open the menu in the top right corner to navigate to other pages.
 				<br/>
 				<br/>
 				You can also use the refresh button to refresh the data displayed throughout the app.
-				Don't stress about your data usage - each refresh only uses around 1000 bytes, which is 0.1% of 1mb! 
+				If you're concerned about data usage, check out the raw facts <Link to='/dashboard/about'>here</Link>.
 			</div>
 		);
 	}
