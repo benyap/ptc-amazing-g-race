58c0500c10e7687f3e596e45cbe46410af86776f
bwyap <bwyap@outlook.com>
2017-10-17 16:32:53 +1100

    [ADD] Add a time stamp to stories

--- src/client/react/components/Story.js
+++ src/client/react/components/Story.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import autobind from 'core-decorators/es/autobind';
+import DateFormat from 'dateformat';
 import MarkdownRenderer from '../../../../lib/react/components/MarkdownRenderer';
 
 import '../scss/components/_story.scss';
@@ -12,7 +13,7 @@ class Story extends React.Component {
 			_id: PropTypes.string.isRequired,
 			type: PropTypes.string.isRequired,
 			createdBy: PropTypes.string.isRequired,
-			publishDate: PropTypes.string.isRequired,
+			publishDate: PropTypes.string,
 			iconName: PropTypes.string.isRequired,
 			intent: PropTypes.string.isRequired,
 			content: PropTypes.string.isRequired,
@@ -27,12 +28,16 @@ class Story extends React.Component {
 
 	render() {
 		const { _id, type, createdBy, publishDate, iconName, intent, content, likes } = this.props.story;
+		let date = publishDate ? new Date(publishDate) : new Date();
 
 		return (
 			<div className={`pt-callout pt-intent-${intent} ${iconName?`pt-icon-${iconName}`:``} story`}>
 				<div className='instruction-panel'>
 					<MarkdownRenderer className='markdown-content' src={content}/>
-					<div style={{marginTop:'0.2rem',display:'flex',flexDirection:'row-reverse'}}>
+					<div style={{margin:'0.2rem 0.2rem 0',display:'flex',justifyContent:'space-between'}}>
+						<small className='pt-text-muted'>
+							{DateFormat(date, 'h:MM TT')}
+						</small>
 						<small className='pt-text-muted'>
 							{createdBy ? `${createdBy}` : `Auto-generated`}
 						</small>
