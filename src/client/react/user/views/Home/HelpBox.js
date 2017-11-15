cf5d81b8b9c3d19809534142d98144ffdd4afb1b
bwyap <bwyap@outlook.com>
2017-11-15 16:23:26 +1100

    [FIX] Fix formatting on help box

--- src/client/react/user/views/Home/HelpBox.js
+++ src/client/react/user/views/Home/HelpBox.js
@@ -7,8 +7,6 @@ const HelpBox = ({showHelp}) => {
 			<div className='pt-callout pt-icon-help pt-intent-primary'>
 				Here you can see an overview of your team's progress.
 				Open the menu in the top right corner to navigate to other pages.
-				<br/>
-				<br/>
 				You can also use the refresh button to refresh the data displayed throughout the app.
 				If you're concerned about data usage, check out the raw facts <Link to='/dashboard/about'>here</Link>.
 			</div>
