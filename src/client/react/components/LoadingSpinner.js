17bf22639485c0c8122ba4a907e9b85bee5746c8
bwyap <bwyap@outlook.com>
2017-11-16 12:57:55 +1100

    [ADD] Add prop to control display of loading text on LoadingSpinner component

--- src/client/react/components/LoadingSpinner.js
+++ src/client/react/components/LoadingSpinner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Spinner, NonIdealState } from '@blueprintjs/core';
 
 import '../../assets/images/logo/logo_gray.png';
@@ -6,10 +7,18 @@ import '../scss/components/_loading-spinner.scss';
 
 
 class LoadingSpinner extends React.Component {
+	static propTypes = {
+		hideText: PropTypes.bool
+	}
+
 	render() {
 		return (
 			<div className='loading-spinner' style={{margin:'3rem 0'}}>
-				<NonIdealState title='Loading...' visual={<Spinner className='pt-large'/>}/>
+				{ this.props.hideText ?
+					<NonIdealState visual={<Spinner className='pt-large'/>}/>
+					:
+					<NonIdealState title='Loading...' visual={<Spinner className='pt-large'/>}/>
+				}
 			</div>
 		);
 	}
